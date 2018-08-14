4586eab370e533c81caf91147703b085afddbbc3
Campbell McKenzie <campbell.mckenzie@aarnet.edu.au>
2018-08-14 14:54:00 +1000

    Added support to modify broker.id etc

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-[![Docker Stars](https://img.shields.io/docker/stars/digitalwonderland/kafka.svg)](https://hub.docker.com/r/digitalwonderland/kafka/) [![Docker Pulls](https://img.shields.io/docker/pulls/digitalwonderland/kafka.svg)](https://hub.docker.com/r/digitalwonderland/kafka/) [![](https://images.microbadger.com/badges/image/digitalwonderland/kafka.svg)](https://microbadger.com/images/digitalwonderland/kafka)
-
 # About:
 
 [Docker](http://www.docker.com/) image based on [openjdk:8-jre-alpine](https://github.com/docker-library/openjdk/blob/master/8-jre/alpine/Dockerfile)
@@ -18,7 +16,7 @@ As a minimum the following environment variables must be set:
 
 
 ```
-docker run -d -e KAFKA_ZOOKEEPER_CONNECT=zk.zookeeper digitalwonderland/kafka
+docker run -d -e KAFKA_ZOOKEEPER_CONNECT=zk.zookeeper camboatdocker/alpine-kafka
 ```
 
 (if you are looking for a clusterable Zookeeper Docker image, feel free to use [digitalwonderland/zookeeper](https://github.com/digital-wonderland/docker-zookeeper))
@@ -31,10 +29,17 @@ Configuration parameters can be provided via environment variables starting with
 2. transformation to lower case
 3. replacing any occurences of ```_``` with ```.```
 
-For example an environment variable ```KAFKA_NUM_PARTITIONS=3``` will result in ```num.partitions=3``` within ```server.properties```.
+For example an environment variable ```KAFKA_NUM_PARTITIONS=3``` will result in ```num.partitions=3``` within ```server.properties```
 
 ### Evaluated Parameters
 
-Any environment variable starting with ```KAFKA_``` and ending with ```_COMMAND``` will be first evaluated and the result saved in an environment variable without the trailing ```_COMMAND```.
+Any environment variable starting with ```KAFKA_``` and ending with ```_COMMAND``` will be first evaluated and the result saved in an environment variable without the trailing ```_COMMAND```
 
-For example an environment variable ```KAFKA_ADVERTISED_HOST_NAME_COMMAND=hostname``` will export ```KAFKA_ADVERTISED_HOST_NAME``` with the value obtained by running ```hostname``` command inside the container.
+For example an environment variable ```KAFKA_ADVERTISED_HOST_NAME_COMMAND=hostname``` will export ```KAFKA_ADVERTISED_HOST_NAME``` with the value obtained by running ```hostname``` command inside the container
+
+### Changelog
+Added support to modify the following Kafka options:
+- broker.id
+- offsets.topic.replication.factor
+- transaction.state.log.replication.factor
+- transaction.state.log.min.isr
--- src/usr/local/sbin/start.sh
+++ src/usr/local/sbin/start.sh
@@ -3,7 +3,15 @@
 # Fail hard and fast
 set -eo pipefail
 
-# Evaluate commands
+echo "Container build date: $(cat /etc/build-date)"
+echo "HOSTNAME found: $(hostname)"
+
+# - Evaluate commands - 
+# Any environment variable starting with KAFKA_ and ending with _COMMAND will be first evaluated
+# and the result saved in an environment variable without the trailing _COMMAND
+# For example an environment variable KAFKA_ADVERTISED_HOST_NAME_COMMAND=hostname will export 
+# KAFKA_ADVERTISED_HOST_NAME with the value obtained by running hostname command inside the container
+
 for VAR in $(env)
 do
   if [[ $VAR =~ ^KAFKA_.*_COMMAND= ]]; then
@@ -27,14 +35,59 @@ if [ -e "${KAFKA_LOCK_FILE}" ]; then
 fi
 
 export KAFKA_LOG_DIRS=${KAFKA_LOG_DIRS:-/var/lib/kafka}
+export KAFKA_BROKER_ID=${KAFKA_BROKER_ID:-FALSE}
 
 # Input new line to not break on concat
 echo "" >> $KAFKA_HOME/config/server.properties
 
-# Enable auto broker ids
-sed -i s/^broker\.id=0/#broker\.id=0/g $KAFKA_HOME/config/server.properties
+# - Tidy Up Default Parameters -
+# broker.id - Check if we are to use auto_generated broker ids
+if [[ "$KAFKA_BROKER_ID" == "FALSE" ]]; then
+  echo "No KAFKA_BROKER_ID found - using auto_generated_id"
+  # Enable auto broker ids by commenting it out
+  sed -i "s/^broker\.id=0/#broker\.id=0/g" $KAFKA_HOME/config/server.properties
+  # Unset the variable so we dont create broker.id=FALSE
+  unset KAFKA_BROKER_ID
+else
+  echo "KAFKA_BROKER_ID found - using id: $KAFKA_BROKER_ID"
+  # Change the default setting
+  sed -i "s/^broker\.id=0/broker\.id=$KAFKA_BROKER_ID/g" $KAFKA_HOME/config/server.properties
+  # Unset it as to not create twice
+  unset KAFKA_BROKER_ID
+fi  
 
-# General config
+# offsets.topic.replication.factor
+if [[ "$KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR" != "" ]]; then
+  # If SET then
+ sed -i "s/^offsets\.topic\.replication\.factor=1/offsets\.topic\.replication\.factor=$KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR/g" \
+   $KAFKA_HOME/config/server.properties
+  # Unset it as to not create twice
+  unset KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR
+fi
+
+# transaction.state.log.replication.factor
+if [[ "$KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR" != "" ]]; then
+  # If SET then
+  sed -i "s/^transaction\.state\.log\.replication\.factor=1/transaction\.state\.log\.replication\.factor=$KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR/g" \
+    $KAFKA_HOME/config/server.properties
+  # Unset it as to not create twice
+  unset KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR
+fi
+
+# transaction.state.log.min.isr
+if [[ "$KAFKA_TRANSACTION_STATE_LOG_MIN_ISR" != "" ]]; then
+  # If SET then
+  sed -i "s/^transaction\.state\.log\.min\.isr=1/transaction\.state\.log\.min\.isr=$KAFKA_TRANSACTION_STATE_LOG_MIN_ISR/g" $KAFKA_HOME/config/server.properties
+  # Unset it as to not create twice
+  unset KAFKA_TRANSACTION_STATE_LOG_MIN_ISR
+fi
+
+# - General config -
+# Configuration parameters can be provided via environment variables starting with KAFKA_ 
+# Any matching variable will be added to Kafkas server.properties by removing the KAFKA_ prefix transformation 
+# to lower case replacing any occurences of _ with .
+# For example an environment variable KAFKA_NUM_PARTITIONS=3 will result in num.partitions=3 within server.properties
+
 for VAR in `env`
 do
   if [[ $VAR =~ ^KAFKA_ && ! $VAR =~ ^KAFKA_HOME ]]; then
